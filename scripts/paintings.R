fb801b674cab5e0db44cc03fd5df06c86dc74876
David Pratt <david@midnightmechanism.com>
2023-09-15 21:59:36 -0500

    Put painting titles seed data in there

--- scripts/paintings.R
+++ scripts/paintings.R
@@ -35,41 +35,6 @@ painting_titles <- c(
 
 # Print the vector to check the titles
 print(painting_titles)
-This code will create a character vector called painting_titles containing the titles of the paintings. You can then use this vector for various purposes in your R code.
-
-# Create a character vector with the titles of the paintings
-painting_titles <- c(
-  "Mona Lisa",
-  "Starry Night",
-  "The Last Supper",
-  "Guernica",
-  "The Persistence of Memory",how much machine learnig
-  "The Birth of Venus",
-  "The Scream",
-  "The Night Watch",
-  "Girl with a Pearl Earring",
-  "The Garden of Earthly Delights",
-  "The School of Athens",
-  "The Starry Night",
-  "American Gothic",
-  "Whistler's Mother (Arrangement in Grey and Black No. 1)",
-  "Les Demoiselles d'Avignon",
-  "The Creation of Adam",
-  "The Sistine Chapel Ceiling",
-  "The Persistence of Memory",
-  "The Birth of Venus",
-  "The Night Watch",
-  "The Girl with a Pearl Earring",
-  "Liberty Leading the People",
-  "The Death of Sardanapalus",
-  "Nighthawks",
-  "Water Lilies",
-  "The Milkmaid",
-  "The Son of Man",
-  "Christina's World",
-  "The Arnolfini Portrait",
-  "The Girl with the Dragon Tattoo"
-)
 
 # Print the vector to check the titles
 print(painting_titles)
@@ -147,6 +112,7 @@ print(artist_first_names)
 print(artist_last_names)
 
 
+paintings = cbind.data.frame(painting_titles, artist_first_names, artist_last_names)
+colnames(paintings) = c("title", "first_name", "last_name")
 
-
-
+write.csv(paintings, "../tables/paintings.csv", row.names = FALSE)
